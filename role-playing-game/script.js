23a00a42f5d41a0949bce47e2443f69a64cf01fe
Djurre <djurreaikema@gmail.com>
2024-01-06 10:40:56 +0100

    step 82

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -21,7 +21,24 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 
-const weapons = [];
+const weapons = [
+    {
+        name: "stick",
+        power: 5
+    },
+    {
+        name: "dagger",
+        power: 30
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100
+    },
+];
 
 const locations = [
     {
