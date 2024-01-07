168ab05cb541a6594ac0dd11ae70ad0c90993245
Djurre <djurreaikema@gmail.com>
2024-01-07 09:11:52 +0100

    step 143

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -272,7 +272,7 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks."
     text.innerText += " You attack it with your " + weapons[currentWeapon].name  + "."
 
-    health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
     healthText.innerText = health;
 
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
