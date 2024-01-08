d9912837af1b27ee93f189dea75373d90c728752
Djurre <djurreaikema@gmail.com>
2024-01-08 12:55:16 +0100

    CC step 83

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -83,5 +83,6 @@ function calculateCalories(e) {
   const surplusOrDeficit = remainingCalories >= 0 ? "Surplus" : "Deficit";
 
   output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
-                      <hr>`;
+                      <hr>
+                      <p>${budgetCalories} Calories Budgeted</p>`;
 }
