9590a77059d9557b7e2ac83dea352a0a0d4f427e
Djurre <djurreaikema@gmail.com>
2024-01-08 12:57:14 +0100

    CC step 85

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -87,4 +87,6 @@ function calculateCalories(e) {
                       <p>${budgetCalories} Calories Budgeted</p>
                       <p>${consumedCalories} Calories Consumed</p>
                       <p>${exerciseCalories} Calories Burned</p>`;
+
+  output.classList.remove("hide");
 }
