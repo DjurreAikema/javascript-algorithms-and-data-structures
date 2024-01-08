ec2e3d7ddb23fca7a44db3330695b4264d71f266
Djurre <djurreaikema@gmail.com>
2024-01-08 10:28:03 +0100

    CC step 23

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -9,14 +9,5 @@ let isError = false;
 
 
 function cleanInputString(str) {
-    const strArray = str.split('');
-    const cleanStrArray = [];
-
-    for (let i = 0; i < strArray.length; i++) {
-
-        if (!['+', '-', " "].includes(strArray[i])) {
-            cleanStrArray.push(strArray[i]);
-        }
-
-    }
+    const regex = /hello/;
 }
