80db1ddd8ce912944f4b9915037c2e76106ee848
Djurre <djurreaikema@gmail.com>
2024-01-08 12:41:13 +0100

    CC step 68

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -58,5 +58,6 @@ function calculateCalories(e) {
   e.preventDefault();
   isError = false;
 
-  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]')
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
 }
