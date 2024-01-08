f605ea1450c1d087314699d34ad35f4b3f0f940b
Djurre <djurreaikema@gmail.com>
2024-01-08 10:46:53 +0100

    CC step 43

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -9,16 +9,17 @@ let isError = false;
 
 
 function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, "");
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
 }
 
 function isInvalidInput(str) {
-    const regex = /\d+e\d+/i;
-    return str.match(regex);
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
 }
 
 function addEntry() {
-    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = ``;
 }
