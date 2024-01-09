c3122a81f36caf0fdd7f2cbebf870b541f874f6f
Djurre <djurreaikema@gmail.com>
2024-01-09 10:35:15 +0100

    MP step 49

--- music-player/script.js
+++ music-player/script.js
@@ -176,6 +176,6 @@ const highlightCurrentSong = () => {
   const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
 
   playlistSongElements.forEach((songEl) => {
-
+    songEl.removeAttribute("aria-current");
   });
 };
