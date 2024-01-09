0984c51191202076e3cf7dffc51e1efe794e0551
Djurre <djurreaikema@gmail.com>
2024-01-09 12:43:00 +0100

    MP step 74

--- music-player/script.js
+++ music-player/script.js
@@ -235,5 +235,6 @@ const deleteSong = (id) => {
 
   if (userData.songs.length === 0) {
     const resetButton = document.createElement('button');
+    const resetText = document.createTextNode("Reset Playlist");
   }
 };
