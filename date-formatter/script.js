ab48e24b07f4d6f3afcde20087dac65113c712b7
Djurre <djurreaikema@gmail.com>
2024-01-09 13:05:23 +0100

    DF step 9

--- date-formatter/script.js
+++ date-formatter/script.js
@@ -6,3 +6,6 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = ``;
