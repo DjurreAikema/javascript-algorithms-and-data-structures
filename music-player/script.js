6ebf32b5715622547eccc53c1624f98679150441
Djurre <djurreaikema@gmail.com>
2024-01-09 12:32:47 +0100

    MP step 63

--- music-player/script.js
+++ music-player/script.js
@@ -206,4 +206,6 @@ const setPlayButtonAccessibleText = () => {
 
 const shuffle = () => {
   userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
 };
