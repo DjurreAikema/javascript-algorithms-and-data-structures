af242cad955f0b21ba7921f33883df3a6b809fb0
Djurre <djurreaikema@gmail.com>
2024-01-09 10:34:38 +0100

    MP step 48

--- music-player/script.js
+++ music-player/script.js
@@ -174,4 +174,8 @@ previousButton.addEventListener("click", playPreviousSong);
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+
+  playlistSongElements.forEach((songEl) => {
+
+  });
 };
