94ae811e8420c03ffaad8daed247bd7d2a1ada0e
Djurre <djurreaikema@gmail.com>
2024-01-09 12:45:19 +0100

    MP step 76

--- music-player/script.js
+++ music-player/script.js
@@ -239,5 +239,8 @@ const deleteSong = (id) => {
 
     resetButton.id = "reset";
     resetButton.ariaLabel = "Reset playlist";
+
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
   }
 };
