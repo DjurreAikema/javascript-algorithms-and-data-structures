8ac5cb9d547f5b3b3d2fc6b852e7ca0ec82f4a02
Djurre <djurreaikema@gmail.com>
2024-01-09 10:49:42 +0100

    MP step 61

--- music-player/script.js
+++ music-player/script.js
@@ -202,3 +202,6 @@ const setPlayButtonAccessibleText = () => {
   const song = userData?.currentSong || userData?.songs[0];
   playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play");
 }
+
+
+const shuffle = () => { };
