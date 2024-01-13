0524ffdf59f924cbf7dc5a550a363b919fc18d11
Djurre <djurreaikema@gmail.com>
2024-01-13 22:41:01 +0100

    TA step 54

--- todo-app/script.js
+++ todo-app/script.js
@@ -115,18 +115,3 @@ const reset = () => {
   taskForm.classList.toggle("hidden");
   currentTask = {};
 };
-
-const myTaskArr = [
-  { task: "Walk the Dog", date: "22-04-2022" },
-  { task: "Read some books", date: "02-11-2023" },
-  { task: "Watch football", date: "10-08-2021" },
-];
-
-localStorage.clear();
-
-localStorage.setItem("data", JSON.stringify(myTaskArr));
-const getTaskArr = localStorage.getItem("data");
-console.log(getTaskArr);
-
-const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
-console.log(getTaskArrObj);
