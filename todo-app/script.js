a8f1e5810b53c200518452c197903159fbf2f439
Djurre <djurreaikema@gmail.com>
2024-01-13 22:14:02 +0100

    TA step 30

--- todo-app/script.js
+++ todo-app/script.js
@@ -39,18 +39,6 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-  const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
-    date: dateInput.value,
-    description: descriptionInput.value,
-  };
-
-  if (dataArrIndex === -1) {
-    taskData.unshift(taskObj);
-  }
-
   taskData.forEach(({ id, title, date, description }) => {
     tasksContainer.innerHTML += `
       <div class="task" id="${id}">
@@ -66,6 +54,20 @@ taskForm.addEventListener("submit", (e) => {
 
   reset();
 });
+
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+};
 
 const reset = () => {
   titleInput.value = "";
