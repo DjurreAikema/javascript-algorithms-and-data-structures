1b992b5fde7b44313b61a23911968e9df65cccd5
Djurre <djurreaikema@gmail.com>
2024-01-13 22:28:12 +0100

    TA step 41

--- todo-app/script.js
+++ todo-app/script.js
@@ -88,6 +88,8 @@ const editTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
+  currentTask = taskData[dataArrIndex];
 };
 
 const reset = () => {
