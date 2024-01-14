e12627bcba6a93b4922ef1b7458db109d6252743
Djurre <djurreaikema@gmail.com>
2024-01-14 09:28:12 +0100

    DBC step 9

--- decimal-to-binary-converter/script.js
+++ decimal-to-binary-converter/script.js
@@ -3,7 +3,8 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-  console.log(numberInput.value);
+  if (numberInput.value === "") {
+  }
 };
 
 convertBtn.addEventListener("click", checkUserInput);
