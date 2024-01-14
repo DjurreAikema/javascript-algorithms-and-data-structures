4f7471286e189dc536078eaa7b92f8d2070724d7
Djurre <djurreaikema@gmail.com>
2024-01-14 10:24:06 +0100

    DBC step 73

--- decimal-to-binary-converter/script.js
+++ decimal-to-binary-converter/script.js
@@ -21,10 +21,8 @@ numberInput.addEventListener("keydown", (e) => {
 });
 
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "0";
-  } else if (input === 1) {
-    return "1";
+  if (input === 0 || input === 1) {
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
