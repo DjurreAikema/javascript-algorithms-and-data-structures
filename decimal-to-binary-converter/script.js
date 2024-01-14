70740c6cfdeaf548b76cffc65074df657de71411
Djurre <djurreaikema@gmail.com>
2024-01-14 10:17:00 +0100

    DBC step 65

--- decimal-to-binary-converter/script.js
+++ decimal-to-binary-converter/script.js
@@ -34,18 +34,3 @@ const decimalToBinary = (input) => {
 
   result.innerText = binary;
 };
-
-
-const countDownAndUp = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    console.log("Reached base case");
-    return;
-  } else {
-    countDownAndUp(number - 1);
-    console.log(number);
-  }
-};
-
-countDownAndUp(3);
