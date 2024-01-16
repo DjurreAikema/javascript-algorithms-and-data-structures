c891487b730a5f4e92c1614f7c5d6ce1ca65aeb2
Djurre <djurreaikema@gmail.com>
2024-01-16 12:03:34 +0100

    SC step 22

--- statistics-calculator/script.js
+++ statistics-calculator/script.js
@@ -4,6 +4,10 @@ const getMean = (array) =>
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
 };
 
 
