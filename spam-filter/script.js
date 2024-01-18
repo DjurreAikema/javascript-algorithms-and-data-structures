09646a7552521f74b4c12efbe1c2ff935d913195
Djurre <djurreaikema@gmail.com>
2024-01-18 10:44:06 +0100

    SF step 5

--- spam-filter/script.js
+++ spam-filter/script.js
@@ -1,14 +1,18 @@
-const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
+const messageInput = document.getElementById("message-input");
 const checkMessageButton = document.getElementById("check-message-btn");
-
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
     return;
   }
-});
 
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+
+  messageInput.value = "";
+});
 
 const isSpam = (msg) => false;
