333d3335e76c5f2e4598e7bd424982b62c622c68
Djurre <djurreaikema@gmail.com>
2024-01-21 14:54:54 +0100

    S step 27

--- spreadsheet/script.js
+++ spreadsheet/script.js
@@ -54,6 +54,7 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      input.onchange = update;
 
       container.appendChild(input);
     });
