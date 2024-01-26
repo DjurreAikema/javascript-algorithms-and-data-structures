0570587f09d8f04774cfc3274bec1f1b7355e1db
Djurre <djurreaikema@gmail.com>
2024-01-26 11:28:08 +0100

    S step 87

--- spreadsheet/script.js
+++ spreadsheet/script.js
@@ -60,7 +60,9 @@ const applyFunction = (str) => {
   const toNumberList = (args) => args.split(",").map(parseFloat);
   const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
 
-  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()));
+  return str2.replace(functionCall, (match, fn, args) =>
+    spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match
+  );
 };
 
 window.onload = () => {
