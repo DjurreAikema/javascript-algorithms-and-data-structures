f8bf3aae49db0d3142827243343a512592a03c5e
Djurre <djurreaikema@gmail.com>
2024-01-26 15:47:42 +0100

    RNC step 6

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -7,4 +7,5 @@ convertBtn.addEventListener("click", () => {
 
   if (!value) output.innerText = "Please enter a valid number";
   if (value < 1) output.innerText = "Please enter a number greater than or equal to 1";
+  if (value > 3999) output.innerText = "Please enter a number less than or equal to 3999";
 });
