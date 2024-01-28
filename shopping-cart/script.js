1ab933c6b7c32d9ac8bdf7bfa46bb725ad9d0ea1
Djurre <djurreaikema@gmail.com>
2024-01-28 12:20:29 +0100

    SC step 44

--- shopping-cart/script.js
+++ shopping-cart/script.js
@@ -134,6 +134,8 @@ class ShoppingCart {
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
   }
+
+  calculateTaxes(amount) {}
 }
 
 const cart = new ShoppingCart();
