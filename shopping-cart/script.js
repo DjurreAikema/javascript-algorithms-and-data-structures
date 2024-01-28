458cb1e2e665537902eceb4d13b01cbdefa038f1
Djurre <djurreaikema@gmail.com>
2024-01-28 12:27:20 +0100

    SC step 51

--- shopping-cart/script.js
+++ shopping-cart/script.js
@@ -137,6 +137,8 @@ class ShoppingCart {
     this.total = subTotal + tax;
 
     cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
   }
 
   calculateTaxes(amount) {
