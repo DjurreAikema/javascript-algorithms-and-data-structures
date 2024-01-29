6e6045d59b68357639779c6a30cb1ae08912d7c5
d.aikema <djurreaikema@gmail.com>
2024-01-29 16:18:24 +0100

    PG step 76

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -110,7 +110,13 @@ const animate = () => {
   }
 
   if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
-    platforms.forEach((platform) => (platform.position.x -= 5));
+    platforms.forEach((platform) => {
+      platform.position.x -= 5;
+    });
+  } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+    platforms.forEach((platform) => {
+      platform.position.x += 5
+    });
   }
 };
 
