fdb3cde18453ac832b8a45ae01f1730de124bb1a
d.aikema <d.aikema@arox.nl>
2024-01-29 15:05:59 +0100

    PG step 52

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -100,8 +100,8 @@ const movePlayer = (key, xVelocity, isPressed) => {
   switch (key) {
     case "ArrowLeft":
       keys.leftKey.pressed = isPressed;
-      if (xVelocity === 0) {
-        player.velocity.x = xVelocity;
-      }
+      if (xVelocity === 0) player.velocity.x = xVelocity;
+      player.velocity.x -= xVelocity;
+      break;
   }
 };
