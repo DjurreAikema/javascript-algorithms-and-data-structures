3baad3da4bbfd4f93084b2726c3e1e7a5b709649
d.aikema <djurreaikema@gmail.com>
2024-01-29 15:48:07 +0100

    PG step 9

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -1,13 +1,16 @@
 // const ... = document.getElementById("");
+// Get elements from the document
 const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 
+// Canvas
 const ctx = canvas.getContext("2d");
-
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+// Game variables
 const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
