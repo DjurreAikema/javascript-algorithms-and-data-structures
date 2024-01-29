1c62fffdfbe7a255f6c7d020f2bd41077452ef7f
d.aikema <djurreaikema@gmail.com>
2024-01-29 16:14:29 +0100

    PG step 75

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -110,6 +110,7 @@ const animate = () => {
   }
 
   if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+    platforms.forEach((platform) => (platform.position.x -= 5));
   }
 };
 
