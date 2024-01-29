85d2cee5540ad51baa40b128fd96919b142cf2de
d.aikema <d.aikema@arox.nl>
2024-01-29 15:02:46 +0100

    PG step 48

--- platformer-game/script.js
+++ platformer-game/script.js
@@ -92,5 +92,8 @@ const keys = {
 
 const movePlayer = (key, xVelocity, isPressed) => {
   if (!isCheckpointCollisionDetectionActive) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+    return;
   }
 };
