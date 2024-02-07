5ef74fbb89c875f551294334d816dd01f4ee0507
Djurre <djurreaikema@gmail.com>
2024-02-07 19:33:56 +0100

    DG step 66

--- dice-game/script.js
+++ dice-game/script.js
@@ -119,7 +119,9 @@ const resetGame = () => {
   rolls = 0;
   round = 1;
 
-  listOfAllDice.forEach((dice, index) => {});
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
 };
 
 const updateScore = (selectedValue, achieved) => {
