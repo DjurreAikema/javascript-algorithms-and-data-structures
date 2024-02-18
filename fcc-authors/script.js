34c88c015c6b8201c653b160ae5f034e950e5378
DjurreAikema <djurreaikema@gmail.com>
2024-02-18 22:14:39 +0100

    FA step 4

--- fcc-authors/script.js
+++ fcc-authors/script.js
@@ -2,5 +2,5 @@ const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json").then((res) => {
-  console.log(res);
+  res.json();
 });
