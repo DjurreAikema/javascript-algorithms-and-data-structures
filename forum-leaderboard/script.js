fdab051828ecd550c29f9fd1138af7561328aacb
DjurreAikema <djurreaikema@gmail.com>
2024-02-19 22:45:37 +0100

    FL step 52

--- forum-leaderboard/script.js
+++ forum-leaderboard/script.js
@@ -31,6 +31,7 @@ const forumCategory = (id) => {
 
   const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
   const linkText = selectedCategory.category;
+  const linkClass = `category ${selectedCategory.className}`;
 };
 
 const timeAgo = (time) => {
