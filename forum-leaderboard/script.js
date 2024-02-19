2b3c1836e82eb1a3ea12b2aa82210b2582453564
DjurreAikema <djurreaikema@gmail.com>
2024-02-19 20:20:48 +0100

    FL step 37

--- forum-leaderboard/script.js
+++ forum-leaderboard/script.js
@@ -34,6 +34,7 @@ const fetchData = async () => {
 const viewCount = (views) => {
   const thousands = Math.floor(views / 1000);
   if (views >= 1000) return `${thousands}k`;
+  return views;
 };
 
 fetchData();
