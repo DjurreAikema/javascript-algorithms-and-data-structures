fd78089725590dc59b849bec76c17919ff1db227
DjurreAikema <djurreaikema@gmail.com>
2024-02-19 16:31:40 +0100

    FA step 29

--- fcc-authors/script.js
+++ fcc-authors/script.js
@@ -39,6 +39,7 @@ const fetchMoreAuthors = () => {
 
   if (authorDataArr.length <= endingIndex) {
     loadMoreBtn.disabled = true;
+    loadMoreBtn.style.cursor = "not-allowed";
     loadMoreBtn.textContent = "No more data to load";
   }
 };
