b4ed0d41f1658a0be4b14f7980d12b7f34e305bd
d.aikema <djurreaikema@gmail.com>
2024-02-28 15:01:47 +0100

    CR step 5

--- cash-register/script.js
+++ cash-register/script.js
@@ -11,7 +11,6 @@ let cid = [
   ["ONE HUNDRED", 100],
 ];
 
-
 const cashInput = document.getElementById("cash");
 const changeDueDiv = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
@@ -19,9 +18,56 @@ const purchaseBtn = document.getElementById("purchase-btn");
 purchaseBtn.addEventListener("click", () => {
   const paymentAmount = cashInput.value;
 
+  amountOfCurrency();
+
   if (paymentAmount < price) {
     alert("Customer does not have enough money to purchase the item");
   } else if (paymentAmount === price) {
     changeDueDiv.innerHTML = "No change due - customer paid with exact cash";
   }
 });
+
+// Calculate how much change to gice
+const calculateChange = (price, payment) => {
+  const difference = price - payment;
+  if (difference <= 0) return;
+};
+
+// Calculate how much of each currency is available
+const amountOfCurrency = () => {
+  cid.forEach((item) => {
+    switch (item[0]) {
+      case "PENNY":
+        item.push(divideAndRound(item[1], 0.01));
+        break;
+      case "NICKEL":
+        item.push(divideAndRound(item[1], 0.05));
+        break;
+      case "DIME":
+        item.push(divideAndRound(item[1], 0.1));
+        break;
+      case "QUARTER":
+        item.push(divideAndRound(item[1], 0.25));
+        break;
+      case "ONE":
+        item.push(divideAndRound(item[1], 1));
+        break;
+      case "FIVE":
+        item.push(divideAndRound(item[1], 5));
+        break;
+      case "TEN":
+        item.push(divideAndRound(item[1], 10));
+        break;
+      case "TWENTY":
+        item.push(divideAndRound(item[1], 20));
+        break;
+      case "ONE HUNDRED":
+        item.push(divideAndRound(item[1], 100));
+        break;
+    }
+  });
+
+  console.log(cid);
+};
+
+const divideAndRound = (input, divider) => Math.round(input / divider);
