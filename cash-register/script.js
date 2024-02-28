1654fe04efc03ee9e92a31cdb89a96c825283286
d.aikema <djurreaikema@gmail.com>
2024-02-28 16:03:50 +0100

    CR step 11

--- cash-register/script.js
+++ cash-register/script.js
@@ -1,14 +1,14 @@
-let price = 19.5;
+let price = 3.26;
 let cid = [
-  ["PENNY", 0.5],
-  ["NICKEL", 0],
-  ["DIME", 0],
-  ["QUARTER", 0],
-  ["ONE", 0],
-  ["FIVE", 0],
-  ["TEN", 0],
-  ["TWENTY", 0],
-  ["ONE HUNDRED", 0],
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
 
 const cashInput = document.getElementById("cash");
@@ -22,7 +22,7 @@ purchaseBtn.addEventListener("click", () => {
 
   if (paymentAmount < price) {
     alert("Customer does not have enough money to purchase the item");
-  } else if (paymentAmount === price) {
+  } else if (paymentAmount == price) {
     changeDueDiv.innerHTML = "No change due - customer paid with exact cash";
   } else {
     calculateChange(price, paymentAmount);
@@ -47,17 +47,14 @@ const calculateChange = (price, payment) => {
     }
   });
 
-  if (difference > 0) {
-    changeDueDiv.innerHTML = "Status: OPEN <br />";
+  if (difference == 0) {
+    changeDueDiv.innerHTML = "Status: OPEN ";
+
+    if (cid.every(isZero)) changeDueDiv.innerHTML = "Status: CLOSED ";
+    else changeDueDiv.innerHTML = "Status: OPEN ";
 
     for (const [key, value] of Object.entries(changeDict)) {
-      changeDueDiv.innerHTML += `${key} $${parseFloat(value).toFixed(2)} <br />`;
-    }
-  } else if (difference == 0) {
-    changeDueDiv.innerHTML = "Status: CLOSED <br />";
-
-    for (const [key, value] of Object.entries(changeDict)) {
-      changeDueDiv.innerHTML += `${key} $${parseFloat(value).toFixed(2)} <br />`;
+      changeDueDiv.innerHTML += `${key}: $${parseFloat(value)} `;
     }
   } else {
     changeDueDiv.innerHTML = `Status: INSUFFICIENT_FUNDS`;
@@ -102,3 +99,5 @@ const amountOfCurrency = () => {
 };
 
 const divideAndRound = (input, divider) => Math.round(input / divider);
+
+const isZero = (item) => item[2] == 0;
