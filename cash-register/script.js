3edcd41175df91dde60970dfadd3e94f7622f459
d.aikema <djurreaikema@gmail.com>
2024-02-28 15:23:12 +0100

    CR step 6

--- cash-register/script.js
+++ cash-register/script.js
@@ -1,4 +1,4 @@
-let price = 1.87;
+let price = 19.5;
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -16,21 +16,42 @@ const changeDueDiv = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
 
 purchaseBtn.addEventListener("click", () => {
+  changeDueDiv.innerHTML = "";
   const paymentAmount = cashInput.value;
-
   amountOfCurrency();
 
   if (paymentAmount < price) {
     alert("Customer does not have enough money to purchase the item");
   } else if (paymentAmount === price) {
     changeDueDiv.innerHTML = "No change due - customer paid with exact cash";
+  } else {
+    changeDueDiv.innerHTML = "Status: OPEN <br />";
+    calculateChange(price, paymentAmount);
   }
 });
 
 // Calculate how much change to gice
 const calculateChange = (price, payment) => {
-  const difference = price - payment;
+  let changeDict = {};
+  let difference = payment - price;
   if (difference <= 0) return;
+
+  cid.reverse().forEach((item) => {
+    if (item[3] < difference) {
+      changeDict[item[0]] = 0;
+
+      while (difference >= item[3]) {
+        changeDict[item[0]] += item[3];
+        difference = difference - item[3];
+      }
+    }
+  });
+
+  console.log(changeDict);
+  for (const [key, value] of Object.entries(changeDict)) {
+    console.log(key, value);
+    changeDueDiv.innerHTML += `${key} $${value} <br />`;
+  }
 };
 
 // Calculate how much of each currency is available
@@ -38,31 +59,31 @@ const amountOfCurrency = () => {
   cid.forEach((item) => {
     switch (item[0]) {
       case "PENNY":
-        item.push(divideAndRound(item[1], 0.01));
+        item.push(divideAndRound(item[1], 0.01), 0.01);
         break;
       case "NICKEL":
-        item.push(divideAndRound(item[1], 0.05));
+        item.push(divideAndRound(item[1], 0.05), 0.05);
         break;
       case "DIME":
-        item.push(divideAndRound(item[1], 0.1));
+        item.push(divideAndRound(item[1], 0.1), 0.1);
         break;
       case "QUARTER":
-        item.push(divideAndRound(item[1], 0.25));
+        item.push(divideAndRound(item[1], 0.25), 0.25);
         break;
       case "ONE":
-        item.push(divideAndRound(item[1], 1));
+        item.push(divideAndRound(item[1], 1), 1);
         break;
       case "FIVE":
-        item.push(divideAndRound(item[1], 5));
+        item.push(divideAndRound(item[1], 5), 5);
         break;
       case "TEN":
-        item.push(divideAndRound(item[1], 10));
+        item.push(divideAndRound(item[1], 10), 10);
         break;
       case "TWENTY":
-        item.push(divideAndRound(item[1], 20));
+        item.push(divideAndRound(item[1], 20), 20);
         break;
       case "ONE HUNDRED":
-        item.push(divideAndRound(item[1], 100));
+        item.push(divideAndRound(item[1], 100), 100);
         break;
     }
   });
