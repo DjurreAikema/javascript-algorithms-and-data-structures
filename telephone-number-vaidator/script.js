3421095a88780c6607fa0402157bbe6c991d8636
d.aikema <djurreaikema@gmail.com>
2024-02-28 13:37:39 +0100

    TNV step 5

--- telephone-number-vaidator/script.js
+++ telephone-number-vaidator/script.js
@@ -2,3 +2,9 @@ const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
+
+
+checkBtn.addEventListener("click", () => {
+  alert("Please provide a phone number");
+  console.log("test");
+});
